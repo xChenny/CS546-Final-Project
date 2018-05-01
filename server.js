53550fba15e17de077b96dbbae540c4649fb51e1
Andrew Chen <andrewchen@Andrews-MacBook-Pro.local>
2018-05-01 11:42:05 -0400

    removed cors

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require("cors")
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(morgan("short"));
-// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.urlencoded({
