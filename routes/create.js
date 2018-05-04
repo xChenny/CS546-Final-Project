13b0d44d6393fc08ee0d63bb45900da2545e5789
kmorel <kmorel@stevens.edu>
2018-05-04 16:54:20 -0400

    Create new file route

--- routes/create.js
+++ routes/create.js
@@ -8,4 +8,10 @@ router.post('/user', async (req, res) => {
   if (await createUser(user, pass)) { res.redirect('http://localhost:3000/dashboard') } else { res.redirect('http://localhost:3000/signup') }
 })
 
+router.post('/file', async (req, res) => {
+  const fileName = req.body.fileName
+  console.log(fileName);
+  res.redirect(`/editor/${fileName}`);
+})
+
 module.exports = router
