5f3a14bfb587208bfb40e89fa44b6842b17d10be
PoLoMoTo <cbarnwel@stevens.edu>
2018-05-04 15:14:47 -0400

    Got editor pulling from S3

--- routes/s3.js
+++ routes/s3.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const AWS = require('aws-sdk')
-AWS.config.loadFromPath('config.json')
+const AWS = require("aws-sdk");
+AWS.config.loadFromPath("config.json");
 const params = {
-  Bucket: 'codoc-data'
-}
+  Bucket: "codoc-data"
+};
 let s3Bucket = new AWS.S3({
   params
-})
+});
 
 // router.post("/", upload.single("file"), async (req, res) => {
 //   const data = {
@@ -26,26 +26,27 @@ let s3Bucket = new AWS.S3({
 //   });
 // });
 
-router.get('/all', async (req, res) => {
+router.get("/all", async (req, res) => {
   const urlParams = {
-    Bucket: 'codoc-data',
-    Prefix: 'testUser-'
-  }
-  s3Bucket.listObjects(urlParams, function (err, data) {
-    if (err) res.json(err)
-    res.json(data.Contents)
-  })
-})
+    Bucket: "codoc-data",
+    Prefix: "testUser-"
+  };
+  s3Bucket.listObjects(urlParams, function(err, data) {
+    if (err) res.json(err);
+    res.json(data.Contents);
+  });
+});
 
-router.get('/:id', async (req, res) => {
-  const urlParams = {
-    Bucket: 'codoc-data',
+router.get("/:id", async (req, res) => {
+  const objectParams = {
+    Bucket: "codoc-data",
     Key: req.params.id
-  }
-  s3Bucket.getSignedUrl('getObject', urlParams, function (err, url) {
-    if (err) res.json(err)
-    res.json([url])
-  })
-})
+  };
+  // console.log(req.params.id);
+  s3Bucket.getObject(objectParams, function(err, data) {
+    if (err) res.json(err);
+    res.json(data.Body.toString("ascii"));
+  });
+});
 
-module.exports = router
+module.exports = router;
