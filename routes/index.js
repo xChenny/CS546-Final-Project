79ab4aef413814489b5bd2e247ea0f1bbc008c16
Andrew Chen <andrewchen@Andrews-MacBook-Pro.local>
2018-05-11 20:40:50 -0400

    got markdown compilation to work thanks to corey

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@
 const auth = require('./auth')
 const create = require('./create')
 const s3 = require('./s3')
+const mdcompile = require('./mdcompile')
 
 // Router combiner: Joins all routers to create the backend
 const routes = app => {
   app.use('/login', auth)
   app.use('/create', create)
   app.use('/s3', s3)
+  app.use('/mdcompile', mdcompile)
 
   app.use('*', (req, res) => {
     res.status(404).send('Page Not Found!')
---
+++ routes/mdcompile.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+var markdown = require("markdown").markdown;
+
+router.post("/compile", (req, res) => {
+  let md = null;
+  if (req.body.md) md = req.body.md;
+  else res.send();
+  console.log(md);
+  res.send(markdown.toHTML(md));
+  console.log(markdown.toHTML(md))
+});
+
+module.exports = router;
